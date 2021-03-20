05fb620e5369ffe0d91634105dabe9b9dd2d107d
Matheus <matheussouzaslv2@gmail.com>
2021-03-20 00:27:03 -0300

    Configurando Ambiente de Testes

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,17 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+})
+
 module.exports = {
-  username: 'root',
-  password: 'password',
-  database: 'tdd',
-  host: 'localhost',
-  dialect: 'mysql',
-  operatorAliases:false,
-  logging:false,
-  define:{
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT || 'mysql',
+  storage:'./__tests__/database.sqlite',
+  operatorAliases: false,
+  logging: false,
+  define: {
     timestamps: false,
     underscored: true,
     underscoredAll: true,
