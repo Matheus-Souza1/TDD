87f0df1d11e173e23a05600df16ec06483a1e9c4
Matheus <matheussouzaslv2@gmail.com>
2021-03-19 23:49:30 -0300

    Estruturando Projeto

---
+++ src/config/database.js
@@ -0,0 +1,15 @@
+module.exports = {
+  username: 'root',
+  password: 'password',
+  database: 'tdd',
+  host: 'localhost',
+  dialect: 'mysql',
+  operatorAliases:false,
+  logging:false,
+  define:{
+    timestamps: false,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
+
